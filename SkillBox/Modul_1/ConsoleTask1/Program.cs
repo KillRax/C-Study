44cf5cf74270ed0d8db8a6371df35df3acad8a8f
KillRax <gadina42@gmail.com>
2023-12-31 13:35:07 +0700

    Completed tasks of Module_1

---
+++ SkillBox/Modul_1/ConsoleTask1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+
+            Console.Write("Hello ");
+            Console.Write("World ");
+            Console.Write("!!!");
+            Console.ReadKey();
+        }
+    }
+}
