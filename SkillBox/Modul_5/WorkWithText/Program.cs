d6904687af931f0d3299e6c1408d9f80dbe1feff
KillRax <gadina42@gmail.com>
2024-03-10 13:40:09 +0700

    Modul5_Done

---
+++ SkillBox/Modul_5/WorkWithText/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithText
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше предложение:");
+            string text = Console.ReadLine();
+
+            List<string> words = SplitTextByWords(text);
+
+            Console.WriteLine("\nРазделенный текст:");
+            OutputWords(words);
+
+            string reversedText = ReverseText(text);
+            Console.WriteLine("\nПеревернутый текст:");
+            Console.WriteLine(reversedText);
+        }
+
+        static List<string> SplitTextByWords(string text)
+        {
+            List<string> words = new List<string>();
+            int indexOfStart = 0;
+            int indexOfEnd = text.IndexOf(" ");
+            
+            while ((text.Length > 0) && (indexOfEnd > 0))
+            {
+                words.Add(text.Substring(indexOfStart, indexOfEnd));
+                text = text.Remove(indexOfStart, indexOfEnd + 1);
+                indexOfEnd = text.IndexOf(" ");
+            }
+            words.Add(text);
+            return words;
+        }
+
+        static void OutputWords(List<string> words)
+        {
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        static string ReverseText(string text)
+        {
+            List<string> words = SplitTextByWords(text);
+            string reversedText = "";
+
+            foreach (string word in words) 
+            {
+                reversedText = word+ " " + reversedText;
+            }
+            reversedText = reversedText.Trim();
+            return reversedText;
+        }
+    }
+}
